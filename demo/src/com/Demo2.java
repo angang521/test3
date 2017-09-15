75e92f69d1af00f26b9c3c3a7b5b71878ee4d55a
angang521 <30524669+angang521@users.noreply.github.com>
2017-09-15 18:27:10 +0800

    Update Demo2.java

--- demo/src/com/Demo2.java
+++ demo/src/com/Demo2.java
@@ -5,7 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=5211314;
-		System.out.println("ÎÒÏë¶àÄãËµ:"+i+"abc");
+		System.out.println(""+i+"abc");
 	}
 
 }
