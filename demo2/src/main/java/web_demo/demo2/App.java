027193d79ea395be2d75ed3428d0e8f6126f6f41
xa <angang985@163.com>
2017-09-15 18:33:56 +0800

    https://github.com/angang521/test3.git

---
+++ demo2/src/main/java/web_demo/demo2/App.java
@@ -0,0 +1,13 @@
+package web_demo.demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
