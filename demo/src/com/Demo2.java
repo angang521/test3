df66b20284e511ddfdef4c2716e06494214e0c30
xa <angang985@163.com>
2017-09-15 16:26:35 +0800

    https://github.com/angang521/test3.git

---
+++ demo/src/com/Demo2.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=5211314;
+		System.out.println("我想多你说:"+i);
+	}
+
+}
